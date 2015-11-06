7580dcca4faea87a50cf04feb73a6a0acbf61927
Sarah Le Cam <sdl83@cornell.edu>
2015-11-06 14:11:05 -0500

    Initial implementation of repl mli

---
+++ repl.mli
@@ -0,0 +1,34 @@
+(* repl.mli:
+    Facilitates interaction between the game and the user.
+      - Parsing
+      - Printing
+ *)
+
+(* Defining possible movement direction *)
+type dir = Up | Down | Left | Right
+and opt = Help of str | Game_State of gamestate
+(* Defining possible commands *)
+and cmd =
+  | Quit
+  | NewGame
+  | Print of opt
+  | Move of dir*int
+
+(* Prompts user for next move *)
+val prompt      : gamestate -> (dir * int)
+
+(* print game *)
+val print_game  : gamestate -> unit
+
+(* print help menu *)
+val print_help  : gamestate -> unit
+
+(* processes the comand and initiates the chnge. Returns the new gamestate *)
+val process     : gamestate -> cmd -> gamestate
+
+(* returns a new game state *)
+val new_game    : gamestate -> gamestate
+
+(* returns the final gamestate and quits the game *)
+val quit        : gamestate -> unit
+
