5e74916a6e08e845cb861531ba2a49c70553ab7b
rsn48 <robynnason@users.noreply.github.com>
2015-11-17 20:41:42 -0500

    Update ai.mli

--- ai.mli
+++ ai.mli
@@ -8,3 +8,6 @@ val setup         : game_board -> (game_board * player)
 (* [next_move gamestate] takes a gamestate and returns a command
 for the next move *)
 val next_move     : gamestate -> (dir * int)
+
+(* Computer chooses its next move *)
+val computer_move : game_board -> player -> (piece*location)
