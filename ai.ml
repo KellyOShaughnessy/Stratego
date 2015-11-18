04e18a01b856d1175e77394f267aac33eb967ab2
rsn48 <robynnason@users.noreply.github.com>
2015-11-17 20:42:07 -0500

    Update ai.ml

--- ai.ml
+++ ai.ml
@@ -4,3 +4,6 @@ open Gamestate
 let setup game_board = failwith "unimplemented"
 
 let next_move gamestate = failwith "unimplemented"
+
+(* Computer chooses its next move *)
+let computer_move  game_board player  = failwith "unimplemented"
