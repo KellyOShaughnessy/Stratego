05278e20ac6c527159e07c871ea6d292882670b6
Nikita <ng354@cornell.edu>
2015-12-03 17:22:59 -0500

    very minor changes to mli comments

--- repl.mli
+++ repl.mli
@@ -17,18 +17,19 @@ and cmd =
   | Move of (piece*location)
 
 (* [prompt gamestate] prompts user for next move
- * - returns a tuple of the next direction and the amount of spaces to move*)
+ * - returns a tuple of the next direction and the amount of spaces to move
+ * parses the input text and gives the command. *)
 val prompt      : gamestate -> cmd
 
 (* [print_game gamestate] prints the gameboard.*)
 val print_game  : gamestate -> unit
 
 (* [print_help gamestate] prints the full help menu with all options.
- * contains all of the instructions and commands *)
+ * contains all of the instructions and commands. a block of text*)
 val print_help  : gamestate -> unit
 
 (* [process gamestate] processes the comand [cmd] and initiates the change.
-* Returns the updated gamestate *)
+* Returns the updated gamestate. *)
 val process     : gamestate -> cmd -> gamestate
 
 (* [new_game gamestate] returns a new, fresh gamestate/gameboard*)
